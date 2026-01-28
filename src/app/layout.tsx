159ef4fec2ce6934b10f1d4d9f5ab69e561c812e
Gin-Fao Daniel Georges <ggfdaniel179@gmail.com>
2026-01-28 18:39:24 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { FirebaseProvider } from '@/firebase/provider';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
+import Header from '@/components/layout/header';
 
 export const metadata: Metadata = {
   title: 'UCCOS Admin',
@@ -18,7 +19,12 @@ export default function RootLayout({
       <body>
         <FirebaseProvider>
           <FirebaseClientProvider>
-            {children}
+            <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
+              <Header />
+              <main className="flex-grow container mx-auto p-4 md:p-8">
+                {children}
+              </main>
+            </div>
           </FirebaseClientProvider>
         </FirebaseProvider>
       </body>
